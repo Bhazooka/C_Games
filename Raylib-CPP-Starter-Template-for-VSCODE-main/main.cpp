3a7da706e51e9fce817b2bab0c889fe555937acd
Baraka <barbuk.bb@gmail.com>
2024-11-03 20:21:05 +0200

    Arena, Player, CPU logic done

--- Raylib-CPP-Starter-Template-for-VSCODE-main/main.cpp
+++ Raylib-CPP-Starter-Template-for-VSCODE-main/main.cpp
@@ -3,41 +3,129 @@
 
 using namespace std;
 
-int main () {
+class Ball {
+    public:
+        float x, y;
+        int speed_x, speed_y;               //speed in x and y axis
+        int radius;
 
-    const int SCREEN_WIDTH = 800;
-    const int SCREEN_HEIGHT = 600;
-    int ball_x = 100;
-    int ball_y = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 15;
+    void Draw() {
+        DrawCircle(x, y, radius, WHITE);     //(x,y,radius,colour)
+    }
 
-    cout << "Hello World" << endl;
+    void Update() {
+        // simulate movement by increasing value of the x & y position
+        x += speed_x;
+        y += speed_y;
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My first RAYLIB program!");
-    SetTargetFPS(60);
-
-    while (WindowShouldClose() == false){
-   
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
-
-        if(ball_x + ball_radius >= SCREEN_WIDTH || ball_x - ball_radius <= 0)
+        if(y + radius >= GetScreenHeight() || y - radius <= 0) 
         {
-            ball_speed_x *= -1;
+            speed_y *= -1;                  //reverse speed if ball hits edge of screen (go in opposite direction)
         }
-
-        if(ball_y + ball_radius >= SCREEN_HEIGHT || ball_y - ball_radius <= 0)
+        if(x + radius >= GetScreenWidth() || x - radius <= 0) 
         {
-            ball_speed_y *= -1;
+            speed_x *= -1;                  //reverse speed if ball hits edge of screen (go in opposite direction)
+        }
+    }
+};
+
+class Paddle {
+    public:
+        float x, y;
+        float width, height;
+        int speed;
+
+    void Draw() {
+        DrawRectangle(x, y, width, height, WHITE);              //FIRST PADDLE (x,y,width,height,colour)
+    }
+
+    void Update() {
+        if(IsKeyDown(KEY_UP)){
+            y -= speed;
+        }
+        if(IsKeyDown(KEY_DOWN)){
+            y += speed;
         }
         
+        if(y <= 0){
+            y = 0;                                              //restrict paddle from passing edge of y border
+        }
+        if(y + height >= GetScreenHeight()){
+            y = GetScreenHeight() - height;
+        }
+    }
+};
+
+
+class CpuPaddle: public Paddle                                  //inheritance
+{                                 
+    public: 
+        void Update(int ball_y)
+        {
+            if(y + height/2 > ball_y){
+                y -= speed;
+            }
+            if(y + height/2 <= ball_y){
+                y += speed;
+            }
+        }
+};
+
+
+//Create Objects
+Ball ball;
+Paddle player;
+CpuPaddle cpu;
+
+
+int main () {
+
+    cout << "Starting the game" << endl;
+    const int screen_width = 1280;
+    const int screen_height = 800;
+
+    InitWindow(screen_width, screen_height, "Pong");            //Function defined raylib.h
+    SetTargetFPS(60);                                           //Game FPS
+
+    //Ball attribute values
+    ball.radius = 20;
+    ball.y = screen_height/2;
+    ball.x = screen_width/2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+    //Player attribute values
+    player.width = 25;
+    player.height = 120;
+    player.x = screen_width - player.width - 10; 
+    player.y = screen_height/2 - player.height/2;
+    player.speed = 6;
+    //CPU attributes
+    cpu.width = 25;
+    cpu.height = 120;
+    cpu.x = 10;
+    cpu.y = screen_height/2 - cpu.height/2;
+    cpu.speed = 6;
+
+
+
+    //Game Loop
+    while(WindowShouldClose() == false ) {                                          //WindowShouldClose checks if close window icon or "Esc" is pressed, then returns true
+        //Drawing coordinate (x,y) = (0,0) starts at top most left corner.
         BeginDrawing();
-            ClearBackground(BLACK);
-            DrawCircle(ball_x,ball_y,ball_radius, WHITE);
+
+        ball.Update();                                                              //before drawing the ball, loop must first update its position
+        player.Update();
+        cpu.Update(ball.y);
+
+        ClearBackground(BLACK);                                                     //Clears the background, so everything redraws. To get rid of objects trace from prev frames
+        DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);          //(startPosX, startPosY, endPosX, endPosY, color)
+        ball.Draw();                                                                
+        player.Draw();
+        cpu.Draw();
+
         EndDrawing();
     }
 
     CloseWindow();
+    return 0;
 }
